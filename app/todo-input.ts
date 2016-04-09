1d2e7655c41840bffe16adbbda44892bd9bb3fe6
ravkovskiy <kos1992@mail.ru>
2016-04-09 17:56:51 +0300

    using events and refs

--- app/todo-input.ts
+++ app/todo-input.ts
@@ -2,6 +2,13 @@ import {Component} from "angular2/core";
 
 @Component({
     selector: 'todo-input',
-    template: `<div>I'm a todo input</div>`
+    template: `<div>
+    <input type="text" #myInput>
+    <button (click)="onClick(myInput.value)">Click me</button>
+    </div>`
 })
-export class TodoInput{}
+export class TodoInput{
+    onClick(value){
+        console.log(value);
+    }
+}
