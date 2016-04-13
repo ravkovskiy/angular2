49a7087358b28d4ef5c226e4102dece8d0546b7a
ravkovskiy <kos1992@mail.ru>
2016-04-13 16:19:32 +0300

    using 'pape' but not working add new deals

---
+++ app/search-pipe.ts
@@ -0,0 +1,9 @@
+import{Pipe} from "angular2/core";
+@Pipe({
+    name: "search"
+})
+export class SearchPipe{
+    transform(value){
+        return value.filter((item)=> item.title.startsWith('s'));
+    }
+}
